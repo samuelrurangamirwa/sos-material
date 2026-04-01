58393927d63bc0beaa2ec29f59b30b23a792d2c2
alusamuel <s.rurangami@alustudent.com>
2026-04-01 12:31:30 +0200

    Add changes

--- index.js
+++ index.js
@@ -14,11 +14,25 @@ app.post("/user", (req, res) => {
   db.query('INSERT INTO users(fullname, username, age, gender) VALUES (?, ?, ?, ?)', 
     [fullname, username, age, gender], 
     (err, result) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.status(201).json({ id: result.insertId, fullname, username, age, gender });
-  });
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.status(201).json({ id: result.insertId, fullname, username, age, gender });
+    });
+});
+
+app.get("/users",(req,res)=>{
+  db.query("SELECT * FROM users",(err,result)=>{
+    if (err) throw err;
+    res.status(200).json(result);
+  })
+});
+
+app.delete("/user/:id",(req,res)=>{
+  db.query("DELETE FROM users WHERE id=?",[req.params.id],(err,result)=>{
+    if (err) throw err;
+    res.status(200).json(result);
+  })
 });
 
 app.listen(port, () => {
